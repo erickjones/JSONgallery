19cbe4c15c1bfd44c942ad6766c75a5e045fe69f
Erick Jones <erick.jones@gmail.com>
2012-10-18 19:21:28 -0300

    code comments

--- js/galeria_controller.js
+++ js/galeria_controller.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+	//Get my JSON data please
 	$.getJSON(
 		'js/galeria_JSON.js',
 		function(data) {
@@ -15,6 +16,7 @@ $(document).ready(function(){
 
 			}
 
+			//Changes both image and Caption on 'firstBlood' and by click event on arrows
 	  		function changeImageAndCaption(){
 	  			theImage.css("display", "none");
 		  		theImage.attr("src", gal[counter][0]);
@@ -24,9 +26,9 @@ $(document).ready(function(){
 		  		theCaption.html(gal[counter][1]);
 		  		theCaption.fadeIn(500);
 	  		}
-
 	  		changeImageAndCaption();
 
+	  		//Right arrow controller
 			$("#rightArrow").click(function() {
 
 		  		if(counter < gal.length-1){
@@ -39,6 +41,7 @@ $(document).ready(function(){
 		  		
 			});
 
+			// Left arrow controller
 			$("#leftArrow").click(function() {
 
 		  		if(counter == 0){
@@ -47,19 +50,14 @@ $(document).ready(function(){
 		  			counter --;
 		  		}
 
-		  		theImage.css("display", "none");
-		  		theImage.attr("src", gal[counter][0]);
-		  		theImage.fadeIn(500);
-
-		  		theCaption.css("display", "none");
-		  		theCaption.html(gal[counter][1]);
-		  		theCaption.fadeIn(500);
+		  		changeImageAndCaption();
 		  		
 			});
 		}
 
 	);
 
+    //Hovering images
 	$("#rightArrow, #leftArrow").hover(function(){
 		$(this).animate({ opacity: "0.8" }, 300);
 	}, function() {
