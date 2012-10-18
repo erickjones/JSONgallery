23bb9b15e91dcb176585f43839e0615c9bd05141
Erick Jones <erick.jones@gmail.com>
2012-10-18 12:22:36 -0300

    first blood

---
+++ galeria_JSON.js
@@ -0,0 +1,50 @@
+{
+   "data":[
+      {
+         "foto":{
+            "imagem":"_img/bola.jpg",
+            "legenda":"Lorem Ipsum Dolor sit amet"
+         },
+         "foto":{
+            "imagem":"_img/bola.jpg",
+            "legenda":"Lorem Ipsum Dolor sit amet"
+         },
+         "foto":{
+            "imagem":"_img/bola.jpg",
+            "legenda":"Lorem Ipsum Dolor sit amet"
+         },
+         "foto":{
+            "imagem":"_img/bola.jpg",
+            "legenda":"Lorem Ipsum Dolor sit amet"
+         },
+         "foto":{
+            "imagem":"_img/bola.jpg",
+            "legenda":"Lorem Ipsum Dolor sit amet"
+         },
+         "foto":{
+            "imagem":"_img/bola.jpg",
+            "legenda":"Lorem Ipsum Dolor sit amet"
+         },
+         "foto":{
+            "imagem":"_img/bola.jpg",
+            "legenda":"Lorem Ipsum Dolor sit amet"
+         },
+         "foto":{
+            "imagem":"_img/bola.jpg",
+            "legenda":"Lorem Ipsum Dolor sit amet"
+         },
+         "foto":{
+            "imagem":"_img/bola.jpg",
+            "legenda":"Lorem Ipsum Dolor sit amet"
+         },
+         "foto":{
+            "imagem":"_img/bola.jpg",
+            "legenda":"Lorem Ipsum Dolor sit amet"
+         },
+         "foto":{
+            "imagem":"_img/bola.jpg",
+            "legenda":"Lorem Ipsum Dolor sit amet"
+         }
+      }
+   ]
+}
---
+++ js/galeria_controller.js
@@ -0,0 +1,11 @@
+var leftArrow = document.getElementById("leftArrow"),
+    rightArrow = document.getElementById("rightArrow");
+
+
+leftArrow.onclick = function(){
+	console.log("Left clicado");
+}
+
+rightArrow.onclick = function(){
+	console.log("Right clicado");
+}
