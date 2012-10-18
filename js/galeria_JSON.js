88e4669e2a52d3f90b5a4c95ee7c1949a438a0b1
Erick Jones <erick.jones@gmail.com>
2012-10-18 16:21:27 -0300

    v2
    += Jquery hover animations in arrow buttons and fadeIn on change
    picture and caption.

--- js/galeria_JSON.js
+++ js/galeria_JSON.js
@@ -1,50 +1,26 @@
 {
-   "data":[
-      {
-         "foto":{
-            "imagem":"_img/bola.jpg",
-            "legenda":"Lorem Ipsum Dolor sit amet"
-         },
-         "foto":{
-            "imagem":"_img/bola.jpg",
-            "legenda":"Lorem Ipsum Dolor sit amet"
-         },
-         "foto":{
-            "imagem":"_img/bola.jpg",
-            "legenda":"Lorem Ipsum Dolor sit amet"
-         },
-         "foto":{
-            "imagem":"_img/bola.jpg",
-            "legenda":"Lorem Ipsum Dolor sit amet"
-         },
-         "foto":{
-            "imagem":"_img/bola.jpg",
-            "legenda":"Lorem Ipsum Dolor sit amet"
-         },
-         "foto":{
-            "imagem":"_img/bola.jpg",
-            "legenda":"Lorem Ipsum Dolor sit amet"
-         },
-         "foto":{
-            "imagem":"_img/bola.jpg",
-            "legenda":"Lorem Ipsum Dolor sit amet"
-         },
-         "foto":{
-            "imagem":"_img/bola.jpg",
-            "legenda":"Lorem Ipsum Dolor sit amet"
-         },
-         "foto":{
-            "imagem":"_img/bola.jpg",
-            "legenda":"Lorem Ipsum Dolor sit amet"
-         },
-         "foto":{
-            "imagem":"_img/bola.jpg",
-            "legenda":"Lorem Ipsum Dolor sit amet"
-         },
-         "foto":{
-            "imagem":"_img/bola.jpg",
-            "legenda":"Lorem Ipsum Dolor sit amet"
-         }
+   "gallery":[
+   
+   {
+      "picture" : {
+         "image":"pictures/bola1.jpg",
+         "_caption":"Lorem Ipsum Dolor sit amet 1"
       }
+   },
+
+   {
+      "picture" : {
+         "image":"pictures/bola2.jpg",
+         "_caption":"Lorem Ipsum Dolor sit amet 2"
+      }
+   },
+
+   {
+      "picture" : {
+         "image":"pictures/bola3.jpg",
+         "_caption":"Lorem Ipsum Dolor sit amet 3"
+      }
+   }
+   
    ]
 }
--- js/galeria_controller.js
+++ js/galeria_controller.js
@@ -1,11 +1,45 @@
-var leftArrow = document.getElementById("leftArrow"),
-    rightArrow = document.getElementById("rightArrow");
+$(document).ready(function(){
 
+	$.getJSON(
+		'js/galeria_JSON.js',
+		function(data) {
 
-leftArrow.onclick = function(){
-	console.log("Left clicado");
-}
+			var gal = [];
+			var counter = 0;
 
-rightArrow.onclick = function(){
-	console.log("Right clicado");
-}
+			for(i=0; i < data.gallery.length; i++){
+
+				gal.push([data.gallery[i].picture.image, data.gallery[i].picture._caption]);
+
+			}
+
+			$("#rightArrow").click(function() {
+
+		  		var theImage = $("#galleryWrapper #imageBox").find("img");
+		  		var theCaption = $("#galleryWrapper").find("#captionBox p");
+
+		  		if(counter < gal.length-1){
+		  			counter++;
+		  		} else {
+		  			counter = 0;
+		  		}
+
+		  		theImage.css("display", "none");
+		  		theImage.attr("src", gal[counter][0]);
+		  		theImage.fadeIn(500);
+
+		  		theCaption.css("display", "none");
+		  		theCaption.html(gal[counter][1]);
+		  		theCaption.fadeIn(500);
+		  		
+			});
+		}
+
+	);
+
+	$("#rightArrow, #leftArrow").hover(function(){
+		$(this).animate({ opacity: "0.8" }, 300);
+	}, function() {
+		$(this).animate({ opacity: "0.5" }, 300);
+	});
+});
